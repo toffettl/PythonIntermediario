3f67f6f7f2246185b15f267b6d7faf86bc7160c6
Felipe Toffetti <139416594+toffettl@users.noreply.github.com>
2025-02-11 21:43:08 -0300

    Métodos Open

--- aula39CriandoArquivos.py
+++ aula39CriandoArquivos.py
@@ -21,3 +21,25 @@ with open(caminho_arquivo, 'w+') as arquivo:
 print('#' * 10)
 with open(caminho_arquivo, 'r') as arquivo:
     print(arquivo.read())
+
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+    arquivo.write('Atenção\n')
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    print('Lendo')
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+print('#' * 10)
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
